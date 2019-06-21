6be52d63bfaa3cd8d20129c2cf3f9a51fa02bac7
Christian Breiderhoff <christian@debianMainVM>
2019-06-21 10:39:43 +0200

    edit tvflow params

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 import copy
 
 
-eps = 0.1  # -e regularization eps (double); default: "0.001"
+eps = 0.001  # -e regularization eps (double); default: "0.001"
 itr = 30  # maximum  iterations (unsigned int); default: "10"
-gradeps = 0.1  # gradient eps (double); default: "0.001"
+gradeps = 0.001  # gradient eps (double); default: "0.001"
 scl = "scale"  # scale output file (string); default: ""
 sclMinItr = 0  # iteration to start measurement (unsigned int);default: "0"
 tau = 1.0  # step size tau (double); default: "1.0"
